c7983a9534cb8aeca77baf8b1fc9744cfae7d3e1
Shirley Gaw <sgaw@users.noreply.github.com>
2015-05-09 14:21:33 +0200

    Choose between a grid and linear layout for credential listing on tablet v phone
    Note sure this actually works as emulators currently broken.

--- app/src/main/java/com/playground/sgaw/credentialsample/credentiallistingsampleapp/CredentialListFragment.java
+++ app/src/main/java/com/playground/sgaw/credentialsample/credentiallistingsampleapp/CredentialListFragment.java
@@ -3,8 +3,10 @@ package com.playground.sgaw.credentialsample.credentiallistingsampleapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,9 @@ import com.playground.sgaw.credentialsample.credentiallistingsampleapp.model.Cre
  * Fragment for viewing a listing of login credentials.
  */
 public class CredentialListFragment extends Fragment {
+    private static final String TAG = "CredentialListFragment";
+    private static final int GRID_SPAN = 2;
+
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -36,8 +41,15 @@ public class CredentialListFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_credential_list, container, false);
         RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.listing_view);
 
-        // TODO(sgaw): Be able to handle GridLinearLayoutManager versus LinearLayoutManager
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        LinearLayoutManager layoutManager;
+        if (rootView.findViewById(R.id.detailContainer) == null) {
+            Log.i(TAG, "LinearLayoutManager");
+            layoutManager = new LinearLayoutManager(getActivity());
+        } else {
+            Log.i(TAG, "GridLayoutManager");
+            layoutManager = new GridLayoutManager(getActivity(), GRID_SPAN);
+        }
+        recyclerView.setLayoutManager(layoutManager);
 
         RecyclerView.Adapter adapter = new CredentialListAdapter(
                 CredentialAgency.get(getActivity()), (CredentialClickListener) getActivity());
