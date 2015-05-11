37fcefa94939dcc4cff8f12dbc9eec3d32c6ab1c
Shirley Gaw <sgaw@users.noreply.github.com>
2015-05-11 10:20:44 +0200

    Correctly initializes agency corpus from dropbox content.

--- app/src/main/java/com/playground/sgaw/credentialsample/credentiallistingsampleapp/model/CredentialAgency.java
+++ app/src/main/java/com/playground/sgaw/credentialsample/credentiallistingsampleapp/model/CredentialAgency.java
@@ -3,9 +3,11 @@ package com.playground.sgaw.credentialsample.credentiallistingsampleapp.model;
 import android.content.Context;
 import android.util.Log;
 
+import com.android.volley.Response;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.playground.sgaw.credentialsample.credentiallistingsampleapp.ContentFetcher;
 
 import java.util.ArrayList;
 
@@ -15,7 +17,9 @@ import java.util.ArrayList;
  * Persists for the application lifecycle (independent of activities, fragments, etc.)
  */
 public class CredentialAgency {
-    private static final String TAG = "sgaw.CredentialAgency";
+    private static final String TAG = "CredentialAgency";
+    private static final String IGNORE_SUFFIX = "@2x";
+    private static final String ICON_SUFFIX = ".png";
     private static CredentialAgency sCredentialAgency;
 
     private Context mAppContext; // Access to resources, storage, etc.
@@ -46,16 +50,56 @@ public class CredentialAgency {
         mDisplayedCredentials = Lists.newArrayList();
     }
 
-    public void init(ListingCorpusListener listener) {
+    public void init(final ListingCorpusListener listener) {
         Log.i(TAG, "init()");
-        ImmutableList.Builder<Credential> builder = ImmutableList.builder();
-        // TODO(sgaw): Fetch login credentials from file.
-        for (int i = 0; i < 100; i++) {
-            builder.add(new Credential(i, String.format("example-%d.com", i)));
+
+        // Data for which credentials are supported needs to be fetched over the network.
+        ContentFetcher.get(mAppContext).requestListingCorpus(new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i(TAG, "Listener.onResponse()");
+                ImmutableList.Builder<Credential> builder = ImmutableList.builder();
+
+                // BUGBUG(sgaw): Mutiple calls to init will result in the old credential ids
+                // will accessing new credential instances.
+                int id = 0;
+                for (String filename : response.split("\n")) {
+                    Log.v(TAG, String.format("Considering file: %s", filename));
+                    String domain = iconFilenameToDomain(filename.trim());
+                    // There are duplicate domains listed
+                    if (!domain.endsWith(IGNORE_SUFFIX)) {
+                        builder.add(new Credential(id, domain));
+                        Log.v(TAG, String.format("Added domain: %s", domain));
+                    }
+                }
+
+                mOriginalCredentials = builder.build();
+                mDisplayedCredentials = Lists.newArrayList(mOriginalCredentials);
+                Log.i(TAG, response);
+                listener.onCorpusDownloaded();
+            }
+        });
+    }
+
+    /**
+     * Returns the icon's filename (format {site}_co_uk.png) with the domain {site}.co_uk
+     *
+     * @param icon_filename
+     * @return Decoded domain name
+     */
+    private String iconFilenameToDomain(String icon_filename) {
+        Log.i(TAG, String.format("iconFilenameToDomain(%s)", icon_filename));
+        StringBuffer buffer = new StringBuffer(icon_filename);
+        buffer.replace(buffer.lastIndexOf(ICON_SUFFIX), icon_filename.length(), "");
+
+        // Decode domains by replacing underscores with periods
+        for (int i = 0; i < buffer.length(); i++) {
+            if (buffer.charAt(i) == '_') {
+                buffer.setCharAt(i, '.');
+            }
         }
-        mOriginalCredentials = builder.build();
-        mDisplayedCredentials = Lists.newArrayList(mOriginalCredentials);
-        listener.onCorpusDownloaded();
+        Log.v(TAG, String.format("domain transformed to %s", buffer.toString()));
+        return buffer.toString();
     }
 
     /**
